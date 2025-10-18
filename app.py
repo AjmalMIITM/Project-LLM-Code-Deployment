cc3d0ab1b88717c3c8bb3de7da21e5c54d4ebcc5
AJMAL M <24f2004489@ds.study.iitm.ac.in>
2025-10-18 10:51:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,16 +56,31 @@ def api_endpoint():
         # 2. Move generated files to project root (overwrite)
         for filename in ["index.html", "README.md"]:
             shutil.copyfile(os.path.join(output_dir, filename), filename)
-        # Always generate LICENSE in the root
+        
+        # FIXED: Always generate COMPLETE MIT LICENSE in the root
         with open("LICENSE", "w") as f:
-            f.write("""MIT License
+            f.write("""
+MIT License
 
-Copyright (c) 2025 Your Name
+Copyright (c) 2025 AjmalMIITM
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell...
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """)
 
         # 3. Read files from root for push
